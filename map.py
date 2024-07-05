09aaa5ccf90ab4343f79974560b20e61371bdd35
capell-ben <capell.geoguy@gmail.com>
2024-07-05 19:43:45 -0400

    basic map of PRCs with pop-ups

--- map.py
+++ map.py
@@ -5,11 +5,17 @@ import folium
 #https://python-visualization.github.io/folium/latest/reference.html
 #m = folium.Map(tiles="cartodb positron")
 #m = folium.Map(location=(49.25, -123.12), tiles="cartodb positron")
-political_countries_url = (
-    "http://geojson.xyz/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson"
+huco_geojson = (
+   r"C:\Users\capel\PyCharmProjs\HuCoPRC\MyProject\s_PDLscrape_FeaturesToJSON.geojson"
 )
+m = folium.Map(location=(40, -100), tiles='Cartodb dark_matter', zoom_start=3)
+folium.GeoJson(huco_geojson,
+                    marker = folium.CircleMarker(radius = 3, # Radius in metres
+                                           weight = 1, #outline weight,
+                                           color = 'white',
+                                           fill_color = '#cc0000',
+                                           fill_opacity = 1),
+               popup=folium.GeoJsonPopup(fields=["PRC_Name", "Address", "Phone_Number", "Services"]),
+               ).add_to(m)
 
-m = folium.Map(location=(30, 10), zoom_start=3, tiles="cartodb positron")
-folium.GeoJson(political_countries_url).add_to(m)
-
-m.save("footprint.html")
+m.save("PregnancyResourceCenters.html")
